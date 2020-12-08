9e8bc734b78815b69edeed1a40401f5333b1973e
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-07 22:39:20 -0700

    added functions, data

--- app.js
+++ app.js
@@ -15,5 +15,61 @@ const connection = mysql.createConnection({
 connection.connect(() => {
   console.log("Welcome to Employee Tracker!");
   // Start my app
-//   postOrBid();
+  whatToDo();
 });
+
+async function whatToDo() {
+  const { whatToDo } = await prompt({
+    name: "whatToDo",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "EXIT",
+    ],
+    type: "list",
+  });
+
+  if (whatToDo === "View All Employees") {
+    viewAllEmp();}
+   else if (whatToDo === "View All Employees By Department") {
+    viewByDepart();
+  } 
+//   else if (whatToDo === "View All Employees By Manager") {
+//     viewByManager();
+//   } else if (whatToDo === "Add Employee") {
+//     addEmployee();
+//   } else if (whatToDo === "Remove Employee") {
+//     viewByDepart();
+//   } else if (whatToDo === "Update Employee Role") {
+//     viewByDepart();
+//   } else if (whatToDo === "Update Employee Manager") {
+//     viewByDepart();
+//   } 
+  else {
+    connection.end();
+  }
+}
+
+function viewAllEmp() {
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if(err) throw err;
+        console.table(res)
+    } )
+}
+async function viewByDepart(){
+    const {viewByDepart} = await prompt({
+       name: "viewDepart",
+       message: "What department would you like to chose?",
+       choices: ["Sales", "Engineering", "Finance", "Legal"],
+       type: "list"
+    })
+
+    
+    connection.query("SELECT * FROM employee WHERE ?")
+}
--- schema.sql
+++ schema.sql
@@ -12,19 +12,16 @@ CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT,
+  department_id INT REFERENCES department.id,
   PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
 	id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
+    role_id INT REFERENCES role.id,
+    manager_id INT REFERENCES employee.id,
     PRIMARY KEY(id),
-    FOREIGN KEY(role_id) REFERENCES role(id),
-    FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
 
